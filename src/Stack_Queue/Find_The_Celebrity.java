930483614d9f754476f71562aaf3abad5898f9e4
aadii-19 <adithpramod197@gmail.com>
2025-06-22 17:06:16 +0530

    Updated Find_The_Celebrity.java

--- src/Stack_Queue/Find_The_Celebrity.java
+++ src/Stack_Queue/Find_The_Celebrity.java
@@ -2,7 +2,13 @@ package Stack_Queue;
 
 public class Find_The_Celebrity {
     public static void main(String[] args) {
+        int[][] people = {
+                {0, 1, 1},
+                {0, 0, 1},
+                {0, 0, 0}  // Person 2 is celebrity
+        };
 
+        System.out.println(find(people));
     }
 
     public static int find(int[][] people){
@@ -21,9 +27,8 @@ public class Find_The_Celebrity {
         }
         if(top>bottom) return -1;
         for(int i=0; i< people.length; i++){
-            if(i!=top && people[top][i]==0 && people[i][top]==1){
-            }
-            else{
+            if(i==top) continue;
+            if(people[top][i]!=0 || people[i][top]!=1){
                 return -1;
             }
         }
